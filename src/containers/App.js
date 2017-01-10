c7d4ce1e4d3245fdb9f6a92e83469832c430c18d
David Kerr <kerrd89@gmail.com>
2017-01-10 07:35:52 -0700

    media-queries

--- src/containers/App.js
+++ src/containers/App.js
@@ -18,7 +18,13 @@ class App extends Component {
     const { mmmHamburger } = this.state;
     return (
       <div className="App">
-        <button onClick={()=>this.setState({ mmmHamburger: !this.state.mmmHamburger })}></button>
+        <button onClick={()=>this.setState({ mmmHamburger: !this.state.mmmHamburger })}>
+
+        <svg width="36px" height="24px" viewBox="3 6 18 12" version="1.1" >
+            <path d="M3,18 L21,18 L21,16 L3,16 L3,18 L3,18 Z M3,13 L21,13 L21,11 L3,11 L3,13 L3,13 Z M3,6 L3,8 L21,8 L21,6 L3,6 L3,6 Z" id="Shape" stroke="none" fill="#FFFFFF" fillRule="evenodd"></path>
+        </svg>
+
+        </button>
         <div className={mmmHamburger ? 'App-sidebar' : 'hidden'}>
           <h2>david kerr</h2>
           <img src={sun} className="App-logo" alt="logo" />
