1ecac18760abb5ce0a9b52f18580f3488de004ed
David Kerr <kerrd89@gmail.com>
2017-01-15 10:17:43 -0700

    added giphy for turing-fridays

--- src/containers/Projects.js
+++ src/containers/Projects.js
@@ -8,11 +8,18 @@ import LinkSvg from "../components/LinkSvg";
 import TwitterApp from "../assets/twitterUserAnalytics.gif";
 import ElectronApp from "../assets/electron-notes-app.gif";
 import TrawnApp from "../assets/trawn.gif";
+import TuringFridays from "../assets/turing-fridays.gif";
 
 const Projects = () => {
   return (
     <div className="Projects">
       <ul>
+        <li><p>Application to manage student led sessions for Turing</p>
+          <img src={TuringFridays} alt="animated gif of notes application" width="80%"/>
+          <span>Using firebase, application allows admins to approve, edit, assign classrooms, and monitor attendence for Friday spike sessions.</span>
+          <GithubSvg link="https://github.com/Jeff-Duke/turing-fridays" />
+          <LinkSvg link="https://turing-fridays.firebaseapp.com/" />
+        </li>
         <li><p>Notes App Using Electron, Vue, and sqlite3</p>
           <img src={ElectronApp} alt="animated gif of notes application" width="80%"/>
           <span>Electron Application using VueJs as the framework, sqlite3 and knexjs for a database, applescript, and text to speech.</span>
